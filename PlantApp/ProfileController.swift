003b8a116ff0cf83e16068bf59982ab5a35e0ce8
Ian Perfitt <ianperfitt@gmail.com>
2018-11-17 16:22:42 -0600

    working on code for getting photos to display on ProfileController

--- PlantApp/ProfileController.swift
+++ PlantApp/ProfileController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
      @IBOutlet var TableView: UITableView!
+    
+    let databaseRef = Database.database().reference() as DatabaseReference!
+    
+    let userID = Auth.auth().currentUser!.uid
+    
+    var downloadLink : String!
+    
+    var downloadLinks = [String]()
     
     @IBAction func addAPlant(_ sender: Any) {
         performSegue(withIdentifier: "AddAPlantSegue", sender: self)
@@ -23,17 +33,36 @@ class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataS
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ProfileControllerTableViewCell
         
-        // TODO
-        //Update nickname, last water date
-        //and image from firebase
-        
         return cell
     }
     
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        databaseRef?.child("users").child(userID).child("usersPhotos").observeSingleEvent(of: .value, with: { snapshot in
+            // check if user has photo
+            for photoURL in snapshot.children.allObjects as! [String] {
+                self.downloadLinks.append(photoURL)
+                
+                }
+            })
+            
+            
+//            if snapshot.hasChild("userPhoto"){
+//                // set image locatin
+//                let filePath = "\(userID)/\("userPhoto")"
+//                // Assuming a < 10MB file, though you can change that
+//                self.storageRef.child(filePath).dataWithMaxSize(10*1024*1024, completion: { (data, error) in
+//                    
+//                    let userPhoto = UIImage(data: data!)
+//                    self.userPhoto.image = userPhoto
+//                })
+//            }
+//        })
+//        
+//    }
     
 
-
+    }
 }
 
